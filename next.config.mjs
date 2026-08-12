c3754013faf7b5ead00040a8ada674207c803cad
SeogjongYu <seogjongyu@gmail.com>
2026-08-12 23:11:41 +0900

    hwp파일 헤더 추가

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,23 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  async headers() {
+    return [
+      {
+        source: "/:path*.hwp",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/x-hwp",
+          },
+          {
+            key: "Content-Disposition",
+            value: "attachment",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-export default nextConfig
+export default nextConfig;
